2bf60af9157f776e5cc6f0335ded49757abde992
Fernando Tarin Morales <icemanf@gmail.com>
2014-01-27 13:52:43 +0900

    Extracted the parser functionality from the BuildRulesTable module to its own and modified it to check if has to parse a restrectied datalog program or not.

---
+++ Datalog/src/Parser.py
@@ -0,0 +1,89 @@
+'''
+Created on Jan 27, 2014
+
+@author: nando
+'''
+
+def removeRuleSpaces(rule):
+    return "".join([x for x in rule if x != " " and x != " \t" and x != "\n"])
+
+def get_predicate(rule, start_position):
+    """ This function is used to parse a predicate of the 
+    form NAME(VAR1,...,VARN) from the start position"""
+    
+    end_name_position = rule.find('(', start_position)
+    if end_name_position == -1:
+        return None, start_position
+    name = rule[start_position:end_name_position]
+    last_var_position = rule.find(')', end_name_position+1)
+    if end_name_position == -1:
+        return None, start_position
+    
+    if rule.find('(', end_name_position+1, last_var_position) != -1:
+        return None, start_position
+    
+    variables = rule[end_name_position+1:last_var_position].split(",")
+    return (name, variables), last_var_position+1
+
+def get_head_separator(rule, start_position):
+    """ Checks that from the starting position what we have is the head rule
+    separator """
+    if start_position+2 >= len(rule) or\
+       rule[start_position:start_position+2] != ':-':
+        return False, start_position
+    else:
+        return True, start_position+2
+    
+def get_predicate_seaparator(rule, start_position):
+    """ Checks that from the starting position what we have is the body rule
+    predicate separator """
+    if start_position+1 >= len(rule) or rule[start_position] != ',':
+        return False, start_position
+    else:
+        return True, start_position+1
+    
+def parseRule(rule, check_restricted=False):
+    """This functions parses a rule. It removes all the spaces of the given
+    rule and parses it returning a tuple of two elements, containing the 
+    head as first element and a list of tuples representing the body as 
+    second element. Both the head and body elements are tuples containing a 
+    string representing the predicate name as a first element, and a list of 
+    strings representing the variables as a second element""" 
+    rule = removeRuleSpaces(rule)
+
+    # Get the predicate head    
+    head, position = get_predicate(rule, 0)
+    if head == None:
+        raise ValueError('Incorrect head')
+    
+    # Check that the next we have in the rule is the separator
+    v, position = get_head_separator(rule, position)
+    if not v:
+        raise ValueError('Head separator not found')
+    
+    # Get the body. Here we can only have two predicates
+    body = []
+    for _ in xrange(2):
+        body_element, position = get_predicate(rule, position)
+        if body_element == None:
+            raise ValueError('Incorrect body')
+        body.append(body_element)
+        
+        # Check if arrived to the end of the rule in an incorrect position
+        if position>=len(rule):
+            raise ValueError('Unfishined rule')
+        
+        # Is this the end of the rule?
+        if rule[position] == '.':
+            break
+        
+        # Get the separator for the second predicate.
+        v, position = get_predicate_seaparator(rule, position)
+        if v == False:
+            raise ValueError('Incorrect body')
+
+    # Check that we don't have more than three hypothesis in the rule
+    if check_restricted and rule[position] != '.':
+        raise ValueError('The body of a rule can at max have only two hypothesis')
+    
+    return (head, body)
