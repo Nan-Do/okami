ed9aded9536b222a911675c54f73057d282d21a6
Fernando Tarin Morales <icemanf@gmail.com>
2014-02-13 16:32:10 +0900

    Added support to test the whole process. That is:    Emitting source code for a given a datalog program.    Compiling the emitted source code.    Executing the solver.    Checking that the results are correct. A 4 Datalog programs to the testing process

--- Datalog/test/FullOperationTests.py
+++ Datalog/test/FullOperationTests.py
@@ -1,37 +1,95 @@
 #!/usr/local/bin/python2.7
 # encoding: utf-8
 
-import os
-import sys
+import os, sys, glob
+import logging
 import subprocess
+import shutil
+
+from itertools import chain
 
 TMP_DIR = '/tmp'
 GENERATED_DIR = 'Solver_C_code'
 COMPILED_FILENAME = 'solver'
 SHOW_SHELL = True
 
-Examples = ['flights.datalog']
+Datalog_Examples = ['flights.datalog',
+                    'graphClausure.dl',
+                    'one-rule.dl',
+                    'pointerAnalysis.dl']
 
-# Get the base directory
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s %(levelname)s %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S")
+
+# Create the directories
 index = os.getcwd().rfind('/')
-working_dir = os.getcwd()[:index] + '/src' 
-
-for example in Examples:
-    os.chdir(working_dir)
-    print "GENERATING: " + example
+base_dir = os.getcwd()[:index]
+compiler_dir = os.path.join(base_dir, 'src')
+solver_dir = os.path.join(TMP_DIR, GENERATED_DIR)
+ 
+for example in Datalog_Examples:
+    # If the solver directory exists remove it
+    if os.path.exists(solver_dir):
+        shutil.rmtree(solver_dir)
+        
+    # Change to the compiler directory and generate the solver for the given
+    # program
+    os.chdir(compiler_dir)
+    logging.info("Emitting code for: " + example)
     command = 'python main.py -d ' + TMP_DIR + ' ../examples/' + example
     subprocess.call(command, shell=SHOW_SHELL)
-    if os.path.isdir(TMP_DIR + '/' + GENERATED_DIR):
-        print "CODE GENERATED SUCCESFULLY"
+    if os.path.isdir(solver_dir):
+        logging.info("Code emitted successfully")
     else:
-        print "CODE NOT GENERATED SUCCESFULLY."
-        print "EXITING"
+        logging.error("Code not emitted.")
+        logging.error("EXITING")
         sys.exit()
         
-    print "COMPILING THE SOURCE CODE"
-    os.chdir(TMP_DIR + '/' + GENERATED_DIR)
+    # Change to the solver directory, compile it and check that the solver was 
+    # created
+    logging.info("Compiling the source code")
+    os.chdir(solver_dir)
     subprocess.call('make', shell=SHOW_SHELL)
-    if os.path.exists(TMP_DIR + '/' + GENERATED_DIR + '/' + COMPILED_FILENAME):
-        print 'COMPILED SUCCESFULLY'
+    if os.path.exists(os.path.join(solver_dir, COMPILED_FILENAME)):
+        logging.info('Compiled successfully')
     else:
-        print 'COMPILATION FAILED'
+        logging.info('Compilation failed')
+        
+    # Copy the input files, execute the solver and check we got the correct 
+    # answers
+    logging.info('Copying the files into compiler directory')
+    data_dir = os.path.join(base_dir, 'examples', example.split('.')[0])
+    input_files =  glob.glob(os.path.join(data_dir, '*.input'))
+    output_files =  glob.glob(os.path.join(data_dir, '*.output'))
+    answer_names = [] 
+    for pos, src in enumerate(chain(input_files, output_files)):
+        name =  os.path.basename(src).split('.')[0]
+        chunk = '.tuples'
+        if pos >= len (input_files):
+            chunk = '-CorrectAnswer.tuples'
+            answer_names.append(name)
+        dst = os.path.join(solver_dir, name + chunk)
+        shutil.copy(src, dst)
+    logging.info('Files copied')
+    logging.info('Executing the solver')
+    subprocess.call('./solver', shell=SHOW_SHELL)
+    logging.info('Solver finished')
+    logging.info('Checking that the answers are correct')
+    
+    # Compare the results with the results that should be obtained
+    for answer in answer_names:
+        if not os.path.exists(answer + '.tuples'):
+            logging.error(answer + '.tuples not generated properly')
+            logging.error("EXITING")
+            sys.exit()
+        p = subprocess.Popen(['diff', answer + '.tuples', answer + '-CorrectAnswer.tuples'], stdout=subprocess.PIPE)
+        
+        if p.stdout.read():
+            logging.error(answer + '.tuples doesn\'t contain the proper answers')
+            sys.exit()
+            
+    logging.info('Generated answers are correct')
+    logging.info('FINISHED\n')
+        
+        
