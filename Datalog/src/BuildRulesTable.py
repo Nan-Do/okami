a306d3f77fa2ce042f1580b8c4a963c68a40a330
Fernando Tarin Morales <icemanf@gmail.com>
2015-11-08 14:10:16 +0900

    Added a new logError function and added some errors to deal with expressions on the rules

--- Datalog/src/BuildRulesTable.py
+++ Datalog/src/BuildRulesTable.py
@@ -7,16 +7,26 @@ Created on Jul 10, 2013
 import sys
 import logging
 
+from collections import defaultdict
+
 from Parser import parseRule
 from Types import LogicRule, PredicateTypes, Predicate
-from collections import defaultdict
+from Types import AssignationExpression, BooleanExpression
+
 
 def addRuleDependencyToGraph(graph, head, body):
     for pred in body:
         #if not graph.has_key(pred):
         #    graph[pred] = dict()
         graph[pred][head] = 0
-         
+
+def logError(filename, line_no, error_type, error_message):
+    logging.error('Analyzing:%s:Line:%i', filename, line_no)
+    if error_type:
+        logging.error('%s:%s', error_type, error_message)
+    else:
+        logging.error('%s', error_message)
+
 # This function checks if a rule is unsafe. If a rule is unsafe can't be 
 # evaluated.
 def isAnUnsafeRule(head, body):
@@ -83,16 +93,49 @@ def buildRulesTable(filename, test=False):
             (head, body) = parseRule(line, check_restricted=True)
         except ValueError as v:
             if not test:
-                logging.error('Parsing:%s:Line:%i', filename, line_no)
-                logging.error('Parsing:%s', v.message)
-
+                logError(filename,
+                         line_no,
+                         'Parsing',
+                         v.message)
             sys.exit(0)
             
         predicates_of_the_body = [x for x in body if isinstance(x, Predicate)]
+        assignation_exp_of_the_body = [x for x in body if isinstance(x, AssignationExpression)]
+        boolean_exp_of_the_body = [x for x in body if isinstance(x, BooleanExpression)]
+        
+        if len(predicates_of_the_body) > 2:
+            logError(filename,
+                     line_no,
+                     'Parsing',
+                     'Rules can\'t contain more than two predicates')
+            sys.exit(0)
+        
+        if len(assignation_exp_of_the_body) > 1:
+            logError(filename,
+                     line_no,
+                     None,
+                     'Only one assignation expression per rule is supported')
+            sys.exit(0)
+        
         if isAnUnsafeRule(head, predicates_of_the_body):
-            logging.error('Analyzing:%s:Line:%i', filename, line_no)
-            logging.error('Analyzing:Unsafe rule')
+            logError(filename,
+                     line_no,
+                     None,
+                     'Unsafe rule')
+            sys.exit(0)
             
+        if len(boolean_exp_of_the_body) > 1:
+            logError(filename,
+                     line_no,
+                     None,
+                     'Only one boolean expression per rule is supported')
+            sys.exit(0)
+        
+        if assignation_exp_of_the_body and boolean_exp_of_the_body:
+            logError(filename,
+                     line_no,
+                     None,
+                     'Simultaneous assignation and a boolean expressions on rules not supported')
             sys.exit(0)
             
         body_predicates_ids = [predicate.id for predicate in predicates_of_the_body]
