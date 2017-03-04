564acb5e80559a52d36e9a1991c63e667c59bb4d
Fernando Tarin Morales <icemanf@gmail.com>
2017-03-04 22:39:56 +0900

    Fixed the generation of the stratum on negated datalog programs

--- Datalog/src/RuleStratifier.py
+++ Datalog/src/RuleStratifier.py
@@ -4,6 +4,8 @@ Created on Dec 18, 2014
 @author: nando
 '''
 
+from Types import Predicate
+
 def updateStratums(predicate_id, rule, stratums):
     # Find the current stratum for the current rule
     stratum_of_rule = 0
@@ -35,10 +37,18 @@ def updateStratums(predicate_id, rule, stratums):
 def stratifyRules(rulesTable):
     stratums = [rulesTable[:]]
     for rule in rulesTable:
-        if rule.body[0].negated:
-            updateStratums(rule.body[0].id, rule, stratums)
-        if rule.type == 2 and rule.body[1].negated:
-            updateStratums(rule.body[1].id, rule, stratums)
+        if rule.type == 1:
+            if type(rule.body[0]) == Predicate and rule.body[0].negated:
+                updateStratums(rule.body[0].id, rule, stratums)
+            if len(rule.body) > 1 and type(rule.body[1]) == Predicate and rule.body[1].negated:
+                    updateStratums(rule.body[1].id, rule, stratums)
+        
+        if rule.type == 2:
+            if type(rule.body[1]) == Predicate and rule.body[1].negated:
+                updateStratums(rule.body[1].id, rule, stratums)
+            if len(rule.body) > 2 and type(rule.body[2]) == Predicate and rule.body[2].negated:
+                updateStratums(rule.body[2].id, rule, stratums)
+
             
 #     for level, stratum in enumerate(stratums):
 #         print "LEVEL", level, ":"
